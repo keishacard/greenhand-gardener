ae4516809f506bf6a9e2618e0ade56ca14b24a7f
Your Name <you@example.com>
2019-06-12 09:12:38 -0500

    login sorta works

--- src/components/Login/loginform.js
+++ src/components/Login/loginform.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import DBcalls from "./DBcalls"
 
 export default class Login extends Component {
 
@@ -17,11 +18,11 @@ export default class Login extends Component {
     handleLogin = e => {
         e.preventDefault()
         //This needs to be a DBcall to check if user exists and log them in
-        api.checkUserThing("email", this.state.password).then(emailResponse => {
-            api.checkUserThing("password", this.state.password).then(passwordResponse => {
+        DBcalls.getUser("email", this.state.password).then(emailResponse => {
+            DBcalls.getUser("password", this.state.password).then(passwordResponse => {
                 //Check to see if username or email are already registered
                 if (nameResponse.length === 0) {
-                    alert("Username, Email, or Password incorrect")
+                    alert("Please try again.")
                 }
                 else {
                     sessionStorage.setItem("credentials", emailResponse[0].id)
