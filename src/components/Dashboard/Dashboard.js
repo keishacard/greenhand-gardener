d64adff08297327d0e74144eebc1ae4504c740ad
Your Name <you@example.com>
2019-06-14 11:09:16 -0500

    abandoning cards for dashboard..... for now

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,72 +11,34 @@ import {
 } from 'reactstrap';
 
 export default class Dashboard extends Component {
+    // clickedCard = () => {
+    //     alert("clicked!")
+    // }
 
     render() {
 
         return (
             <React.Fragment>
                 <h1>Hello [USERNAME], what do you dig?</h1>
-                <div>
-                    <Card>
+                {/* <div>
+                    <Card onClick={this.clickedCard}>
                         <CardImg top width="100%" src={salsaDance} alt="Card image cap" />
                         <CardBody>
                             <CardTitle>Salsa Dance</CardTitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
                         </CardBody>
                     </Card>
+                </div> */}
+                <div>
                 </div>
                 <div>
-                    <Card>
-                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Bartender's Buddy</CardTitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
                 </div>
                 <div>
-                    <Card>
-                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Not Your Grandma's Tea Garden</CardTitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
                 </div>
                 <div>
-                    <Card>
-                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Olive Garden Garden</CardTitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
                 </div>
                 <div>
-                    <Card>
-                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Feed Your Friends</CardTitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
                 </div>
                 <div>
-                    <Card>
-                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>B.Y.O.</CardTitle>
-                            <CardSubtitle>Build Your Own!</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                            <Button>Button</Button>
-                        </CardBody>
-                    </Card>
                 </div>
             </React.Fragment>
         )
