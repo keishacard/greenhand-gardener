92f4a23239b76e4805a2610384d3c0bc8e11a167
Your Name <you@example.com>
2019-06-14 13:10:07 -0500

    images as buttons instead of cards

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,44 +1,49 @@
 import React, { Component } from 'react';
 import salsaDance from "../img/salsa-dance.jpg"
-import {
-    Card,
-    CardImg,
-    CardText,
-    CardBody,
-    CardTitle,
-    CardSubtitle,
-    Button,
-} from 'reactstrap';
+import "./Dashboard.css"
+import DBcalls from "../DBcalls"
 
 export default class Dashboard extends Component {
-    // clickedCard = () => {
-    //     alert("clicked!")
-    // }
+
+    state = {
+        user: {}
+    }
+
+    componentDidMount = () => {
+        DBcalls.getUser(sessionStorage.getItem("credentials")).then(res => {
+            this.setState({ user: res })
+        })
+        console.log(this.state.user)
+    }
+
+    clickedCard = () => {
+        alert("you clicked me")
+    }
 
     render() {
 
         return (
             <React.Fragment>
-                <h1>Hello [USERNAME], what do you dig?</h1>
-                {/* <div>
-                    <Card onClick={this.clickedCard}>
-                        <CardImg top width="100%" src={salsaDance} alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Salsa Dance</CardTitle>
-                        </CardBody>
-                    </Card>
-                </div> */}
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
+                <h1 align="center">Hello {this.state.user.email}, what do you dig?</h1>
+                <div className="garden-container">
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
+                    <div className="garden-card">
+                        <img src={salsaDance} alt="salsa dance" onClick={this.clickedCard} />
+                    </div>
                 </div>
             </React.Fragment>
         )
--- src/components/Gardens/gardenCard.js
+++ src/components/Gardens/gardenCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "./node_modules/react";
-import DBcalls
+// import DBcalls
 
 
 
