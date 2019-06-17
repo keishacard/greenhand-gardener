37cabeacbb1f01ce243a562a16ec5aa256ec70f4
Your Name <you@example.com>
2019-06-17 11:28:32 -0500

    added css to make buttons on dashboard look like they've been pressed looks cuteee

--- src/components/Gardens/gardenCard.js
+++ src/components/Gardens/gardenCard.js
@@ -38,7 +38,62 @@ export default class gardenCard1 extends Component {
                         <CardBody>
                             <CardTitle>Salsa Dance</CardTitle>
                             <CardSubtitle></CardSubtitle>
-                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch!</CardText>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardImg top width="100%" src="https://" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Salsa Dance</CardTitle>
+                            <CardSubtitle></CardSubtitle>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardImg top width="100%" src="https://" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Salsa Dance</CardTitle>
+                            <CardSubtitle></CardSubtitle>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardImg top width="100%" src="https://" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Salsa Dance</CardTitle>
+                            <CardSubtitle></CardSubtitle>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardImg top width="100%" src="https://" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Salsa Dance</CardTitle>
+                            <CardSubtitle></CardSubtitle>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div>
+                    <Card>
+                        <CardImg top width="100%" src="https://" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Salsa Dance</CardTitle>
+                            <CardSubtitle></CardSubtitle>
+                            <CardText>Grow everything you need to make delicious pico de gallo, salsa verde, or salsa rojo from scratch! A variety of peppers means you can tailor your salsas to your perfect level of spiciness.</CardText>
                             <Button>Button</Button>
                         </CardBody>
                     </Card>
