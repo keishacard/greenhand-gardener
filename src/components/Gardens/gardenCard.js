d39aa398064c30e417e3fd470930d542b513308e
Your Name <you@example.com>
2019-06-17 11:08:42 -0500

    gardens

--- src/components/Gardens/gardenCard.js
+++ src/components/Gardens/gardenCard.js
@@ -36,7 +36,7 @@ export default class gardenCard1 extends Component {
                     <Card>
                         <CardImg top width="100%" src="https://" alt="Card image cap" />
                         <CardBody>
-                            <CardTitle>Card title</CardTitle>
+                            <CardTitle>Salsa Dance</CardTitle>
                             <CardSubtitle>Card subtitle</CardSubtitle>
                             <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                             <Button>Button</Button>
